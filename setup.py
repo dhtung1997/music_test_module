165931f89063f7e33a68d1966472d7ebb67abb7c
dhtung1997 <70888931+dhtung1997@users.noreply.github.com>
2020-12-25 15:31:23 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="music_test_module",
+    version="0.0.1",
+    author="DHTung",
+    author_email="dhtung161997@gmail.com",
+    python_requires='>3.6',
+)
