b8b17b54d716e3c5f571223c4d2f037507c351a5
Do Tung <dhtung161997@gmail.com>
2020-12-26 13:45:34 +0700

    commit

--- music_test_module/test.py
+++
@@ -1,4 +0,0 @@
-from music import print_hi
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
---
+++ test.py
@@ -0,0 +1,5 @@
+from music_test_module import music
+
+
+if __name__ == '__main__':
+    music.print_hi()
